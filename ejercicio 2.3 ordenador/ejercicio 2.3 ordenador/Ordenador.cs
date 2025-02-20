ad8e6983955eb46f4ceca29c2c79c9ececb3ab8b
JorgxVillatoro <villatorojorg@gmail.com>
2025-02-20 07:49:04 -0600

    Add files via upload

---
+++ ejercicio 2.3 ordenador/ejercicio 2.3 ordenador/Ordenador.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2._3_ordenador
+{
+    internal class Ordenador
+    {
+        // Atributos (propiedades)
+        public string Marca { get; set; }
+        public string Procesador { get; set; }
+        public int Memoria { get; set; }
+        public int DiscoDuro { get; set; }
+
+        // Constructor
+        public Ordenador(string marca, string procesador, int memoria, int discoDuro)
+        {
+            Marca = marca;
+            Procesador = procesador;
+            Memoria = memoria;
+            DiscoDuro = discoDuro;
+        }
+
+       
+        public override string ToString()
+        {
+            return $"Marca: {Marca}, Procesador: {Procesador}, Memoria: {Memoria}GB, Disco Duro: {DiscoDuro}GB";
+        }
+
+      
+        public void MayorCapacidad()
+        {
+            DiscoDuro += 100;
+            Console.WriteLine($"Nuevo tamaño del disco duro: {DiscoDuro}GB");
+        }
+
+        //  reducir la capacidad del disco duro
+        public void MenorCapacidad()
+        {
+            if (DiscoDuro > 100)
+            {
+                DiscoDuro -= 100;
+                Console.WriteLine($"Nuevo tamaño del disco duro: {DiscoDuro}GB");
+            }
+            else
+            {
+                Console.WriteLine("No se puede reducir más el tamaño del disco duro.");
+            }
+
+        }
+    }
+}
---
+++ ejercicio 2.3 ordenador/ejercicio 2.3 ordenador/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2._3_ordenador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  objeto de la clase Ordenador
+        Ordenador ordenador = new Ordenador("HP", "Intel i7", 16, 500);
+
+            // Mostrar los datos del ordenador
+            Console.WriteLine(ordenador);
+
+            // Incrementar la capacidad del disco duro
+            ordenador.MayorCapacidad();
+
+            // Reducir la capacidad del disco duro
+            ordenador.MenorCapacidad();
+
+            // Mostrar los datos actualizados 
+            Console.WriteLine(ordenador);
+        }
+    }
+}
