7fd1afe2a59b79e129035ffbc31af596b7d8a131
JorgxVillatoro <villatorojorg@gmail.com>
2025-02-20 08:09:50 -0600

    Add files via upload

---
+++ Evaluacion-jorge/Evaluacion-jorge/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluacion_jorge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double totalpupusasfq = 0;
+                if (!string.IsNullOrEmpty(textBox1.Text))
+                {
+                    int cantidadpupusasfq = Convert.ToInt32(textBox1.Text);
+                    totalpupusasfq = cantidadpupusasfq * 0.75;
+                }
+
+                double totalpupusaschica = 0;
+                if (!string.IsNullOrEmpty(textBox2.Text))
+                {
+                    int cantidadpupusaschica = Convert.ToInt32(textBox2.Text);
+                    totalpupusaschica = cantidadpupusaschica * 0.75;
+                }
+
+                double totalpupusasayo = 0;
+                if (!string.IsNullOrEmpty(textBox3.Text))
+                {
+                    int cantidadpupusasayo = Convert.ToInt32(textBox3.Text);
+                    totalpupusasayo = cantidadpupusasayo * 0.80;
+                }
+
+                double totalbebidasoda = 0;
+                if (!string.IsNullOrEmpty(textBox4.Text))
+                {
+                    int bebidasoda = Convert.ToInt32(textBox4.Text);
+                    totalbebidasoda = bebidasoda * 1.00;
+                }
+
+                double totalbebidasagua = 0;
+                if (!string.IsNullOrEmpty(textBox5.Text))
+                {
+                    int bebidasagua = Convert.ToInt32(textBox5.Text);
+                    totalbebidasagua = bebidasagua * 1.00;
+                }
+
+                double totalventa = totalpupusasfq + totalpupusaschica + totalpupusasayo + totalbebidasoda + totalbebidasagua;
+                textBox6.Text = totalventa.ToString("0.00");
+            }
+
+            
+
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingrese valores numéricos válidos.", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        } 
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+   
+
+}
---
+++ Evaluacion2-jorge/Evaluacion2-jorge/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluacion2_jorge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double nota1Teoria = double.Parse(textBox1.Text);
+            double nota2Teoria = double.Parse(textBox2.Text);
+            double nota3Teoria = double.Parse(textBox3.Text);
+            double nota1Lab = double.Parse(textBox4.Text);
+            double nota2Lab = double.Parse(textBox5.Text);
+            double nota3Lab = double.Parse(textBox6.Text);
+
+            if (nota1Teoria < 0 || nota1Teoria > 10 ||
+               nota2Teoria < 0 || nota2Teoria > 10 ||
+               nota3Teoria < 0 || nota3Teoria > 10 ||
+               nota1Lab < 0 || nota1Lab > 10 ||
+               nota2Lab < 0 || nota2Lab > 10 ||
+               nota3Lab < 0 || nota3Lab > 10)
+            {
+                MessageBox.Show("Alguna de las notas ingresadas no está en el rango válido (0.00 a 10.00).");
+                return;
+            }
+
+            // Calcular el promedio final
+            double promedioTeoria = (nota1Teoria + nota2Teoria + nota3Teoria) / 3;
+            double promedioLab = (nota1Lab + nota2Lab + nota3Lab) / 3;
+            double promedioFinal = (promedioTeoria + promedioLab) / 2;
+
+            // Determinar si el estudiante aprobó o reprobó
+            string resultadoFinal;
+            string imagenResultado;
+            if (promedioFinal >= 7.0)
+            {
+                resultadoFinal = "Grandioso, Excelente... continúa así...";
+                imagenResultado = "C:\\Users\\villa\\Downloads\\felicidades.jpg";
+                pictureBox2.ImageLocation = imagenResultado;
+            }
+            else if (promedioFinal >= 6.0)
+            {
+                resultadoFinal = "Muy bien... pero trata de mejorar tu promedio";
+                imagenResultado = "C:\\Users\\villa\\Downloads\\bien.jpg";
+                pictureBox2.ImageLocation = imagenResultado;
+            }
+            else if (promedioFinal >= 3.0)
+            {
+                resultadoFinal = "Más dedicación estudiante, debe mejorar sus notas";
+                imagenResultado = "C:\\Users\\villa\\Downloads\\mejore.jpg";
+                pictureBox2.ImageLocation = imagenResultado;
+            }
+            else
+            {
+                resultadoFinal = "Pongase las pilas mi hijito";
+                imagenResultado = "C:\\Users\\villa\\Downloads\\mal.gif";
+                pictureBox2.ImageLocation = imagenResultado;
+            }
+
+            // Mostrar los resultados
+            textBox7.Text = promedioFinal.ToString("0.00");
+            textBox8.Text = resultadoFinal;
+            
+
+
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+  
